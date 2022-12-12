aee03a1b392f7e9a48d5753adac115dfe1a9cf64
umidjonburkhonov <umedburxonov99@gmail.com>
2022-12-12 10:05:43 +0100

    first commit

---
+++ Umidjon/java script/script.js
@@ -0,0 +1,55 @@
+'use scrict'
+
+//const channel= prompt ("salom hammaga");
+
+//console.log (`https://youtube.com/${channel}+ "/videos"`);
+
+//const user= "Umidjon";
+//const job="Programmistchi";
+
+//console.log(`mein Name ist ${user}, ich bin ${job}`);
+
+
+//const serialOfNames =+prompt ("nechta serial kordingiz", "");
+
+//const serialDB= {
+
+  //  count: serialOfNames,
+   // serials:{},
+   // actiors:{},
+  //  genders:[],
+  //  private:false,
+ 
+
+//};
+
+//const umid1=prompt("Oxirgi serial?"),
+// umid2=prompt("baho bering"),
+// umid3=prompt("Oxirgi serial?"),
+ //umid4=prompt("baho bering");
+
+// serialDB.serials[umid1]=umid2;
+ //serialDB.serials[umid3]=umid4;
+
+
+//console.log(serialDB);
+
+
+
+//let startNum= 1;
+
+//while(startNum 20) {
+// console.log(startNum);
+ //   startNum--;
+//}
+
+
+console.log("hello wordl");
+
+function startApp(text) {
+console.log(text);
+}
+
+startApp("salom hammaga");
+startApp("salom menga");
+startApp("salom unga");
---
+++ htp/js/script.js
@@ -0,0 +1,34 @@
+'use scrict';
+
+//const place = confirm ("are you here");
+//console.log(place);
+
+//const currentPlace = prompt ("Where are you?", "UMId    ");
+//console.log(typeof currentPlace);
+
+//const age = +prompt ("How old are you ?", "18");
+//console.log( age + 10);
+
+//const favouriteColor = [];
+//favouriteColor[0] = prompt ("what is your favourite Color #1", "");
+//favouriteColor[1] = prompt ("what is your favourite Color #2", "");
+//favouriteColor[2] = prompt ("what is your favourite Color #3", "");
+//favouriteColor[3] = prompt ("what is your favourite Color #4", "");
+//console.log(typeof favouriteColor);
+
+//const channel = prompt ("what is your favourite youtubber");
+//console.log(`https:youtube.com ${channel}` );
+
+
+    //const user = "Umidjon";
+    //const userLastname = "BUrkhonov";
+   //const job = "Frontend Developer";
+                                
+    //console.log(`user name is ${user} heir lastname is ${userLastname}, he is ${job}`); 
+
+    //let incr = 10 ;
+    //let decr = 10 ;
+
+    //console.log(incr++);
+   // console.log(decr--);
+
