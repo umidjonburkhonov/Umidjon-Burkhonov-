f523c74ab3bb82644ec517e7c020616994b4d00d
umidjonburkhonov <umedburxonov99@gmail.com>
2022-12-14 23:04:54 +0100

    add new file

--- htp/js/script.js
+++ htp/js/script.js
@@ -1,4 +1,39 @@
-'use scrict';
+"use scrict";
+
+//    const numberOfSeries = +prompt ("Nechta serial ko'rdingiz ?", "");
+
+//    const seriesDB = {
+//     count: numberOfSeries,
+//     series: {},
+//     actors: {},
+//     genders:[],
+//     private: false,
+//    };
+
+//     for (let i = 0; i < 2; i++){
+//         const a = prompt ("Oxirgi ko'rgan serialingiz"),
+//               b =prompt ("nechi baxo berasiz?");
+
+//         if (a != null && b != null && a != ""  && b != ""){
+//             seriesDB.series[a] = b;
+//             console.log("done");
+//         } else {
+//             console.log("error");
+//             i--;
+//         }
+//     }
+
+//     if(seriesDB.count < 5){
+//         console.log("Kam seral ko'ribsiz");
+//     } else if (seriesDB.count >= 5 && seriesDB.count < 10){
+//         console.log("Siz classik tomoshabin ekansiz");
+//     }else if (seriesDB.count >= 10){
+//         console.log("Siz serialchi zvezda ekansiz");
+//     }else {
+//         console.log("Error");
+//     }
+
+//     console.log(seriesDB);
 
 //const place = confirm ("are you here");
 //console.log(place);
@@ -19,16 +54,73 @@
 //const channel = prompt ("what is your favourite youtubber");
 //console.log(`https:youtube.com ${channel}` );
 
+//const user = "Umidjon";
+//const userLastname = "BUrkhonov";
+//const job = "Frontend Developer";
 
-    //const user = "Umidjon";
-    //const userLastname = "BUrkhonov";
-   //const job = "Frontend Developer";
-                                
-    //console.log(`user name is ${user} heir lastname is ${userLastname}, he is ${job}`); 
+//console.log(`user name is ${user} heir lastname is ${userLastname}, he is ${job}`);
 
-    //let incr = 10 ;
-    //let decr = 10 ;
+//let incr = 10 ;
+//let decr = 10 ;
 
-    //console.log(incr++);
-   // console.log(decr--);
+//console.log(incr++);
+// console.log(decr--);
 
+// const age = +prompt ("How old are you ?", "");
+
+// if(age > 18) {
+//     console.log("Welcome");
+// }else {
+//     console.log("no entry");
+// }
+
+// if(age > 25){
+//     console.log("Horror fims");
+// }else if (age > 18 && age < 25){
+//     console.log("boyewik fims");
+// }else {
+//     console.log("Cartons");
+// }
+
+// age > 25 ?   console.log("Horror fims") : console.log("Cartons");
+
+// const color = "salom";
+
+// switch(color){
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "green":
+//         console.log("Goo");
+//         break;
+//     case "yellow":
+//         console.log("Slowly");
+//         break;
+//     default:
+//         console.log("traffic jam");
+//         break;
+// }
+
+// let num = 10;
+
+// while (startNum <= 20){
+//     console.log(startNum);
+//     startNum++;
+// }
+
+// do{
+//     console.log(startNum);
+//     startNum++;
+// } while(startNum <11);
+
+// for (let i = 1; i <= 15; i++){
+//   if (i === 8){
+//     // break;
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// const user = "Umidjon Burkhonov";
+
+// console.log(user.substr(8, 2));
